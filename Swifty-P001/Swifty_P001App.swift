c810bced36987d1bc5c89742c8b480cd0135c47f
Moei <mohammad_sadeghi96@yahoo.com>
2021-01-16 19:13:43 +0100

    Initial Commit

---
+++ Swifty-P001/Swifty_P001App.swift
@@ -0,0 +1,17 @@
+//
+//  Swifty_P001App.swift
+//  Swifty-P001
+//
+//  Created by Mohammadreza Sadeghi on 16/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct Swifty_P001App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
